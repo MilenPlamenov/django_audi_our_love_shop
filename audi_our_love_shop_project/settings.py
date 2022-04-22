eeebec9c365eba148b5e33372af504e9750bcb32
MilenPlamenov <milenkrasy@abv.bg>
2022-04-22 12:17:04 +0300

    Still working on the view of the main page / added buttons for the authentication

--- audi_our_love_shop_project/settings.py
+++ audi_our_love_shop_project/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,7 +91,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+]
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
