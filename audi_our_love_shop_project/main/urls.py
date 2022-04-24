4e13897128954765b0ea8cdd1dacafb908f7c7c1
MilenPlamenov <milenkrasy@abv.bg>
2022-04-24 14:35:27 +0300

    Added about page for the application

--- audi_our_love_shop_project/main/urls.py
+++ audi_our_love_shop_project/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from audi_our_love_shop_project.main.views import index
+from audi_our_love_shop_project.main.views import index, about
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('about/', about, name='about'),
 ]
--- audi_our_love_shop_project/main/views.py
+++ audi_our_love_shop_project/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'base.html')
+
+
+def about(request):
+    return render(request, 'about/about.html')
