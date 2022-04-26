be622e276df079e77e08331a5f2f45d7c5487f4c
MilenPlamenov <milenkrasy@abv.bg>
2022-04-26 14:00:30 +0300

    Finished with the contacts form

--- audi_our_love_shop_project/settings.py
+++ audi_our_love_shop_project/settings.py
@@ -110,12 +110,3 @@ AUTH_USER_MODEL = 'shop_authentication.ShopUser'
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'test934123@gmail.com'
-EMAIL_HOST_PASSWORD = 'obichamvkusnamusaka'
-DEFAULT_FROM_EMAIL = 'test@test.com'
