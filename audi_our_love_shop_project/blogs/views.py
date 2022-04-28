1205895888b4352440243647550f94bd8e9d84d2
MilenPlamenov <milenkrasy@abv.bg>
2022-04-28 11:36:24 +0300

    Fixed the bug when no blogs

--- audi_our_love_shop_project/blogs/views.py
+++ audi_our_love_shop_project/blogs/views.py
@@ -6,9 +6,17 @@ from audi_our_love_shop_project.blogs.models import Blog
 def blog(request):
     blogs = Blog.objects.all()
     last_blog = blogs.last()
-    all_blogs_without_the_last_blog = Blog.objects.all()[0:len(blogs) - 1]
-    context = {
-        'blogs': all_blogs_without_the_last_blog,
-        'last_blog': last_blog,
-    }
+    try:
+        all_blogs_without_the_last_blog = Blog.objects.all()[0:len(blogs) - 1]
+        context = {
+            'blogs': all_blogs_without_the_last_blog,
+            'last_blog': last_blog,
+            'all_blogs': blogs,
+        }
+    except ValueError:
+        context = {
+            'last_blog': last_blog,
+            'all_blogs': blogs,
+        }
+    # could be refactored
     return render(request, 'blogs/blogs.html', context)
