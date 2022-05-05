ab6c7aa7fdced0a57a5b6d90c695bc8b358b5030
MilenPlamenov <milenkrasy@abv.bg>
2022-05-05 17:08:33 +0300

    added remove one and multiple items from the cart views ,messages when adding or removing from the cart

--- audi_our_love_shop_project/shop/urls.py
+++ audi_our_love_shop_project/shop/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from audi_our_love_shop_project.shop.views import home, checkout, ProductDetailView, add_to_cart
+from audi_our_love_shop_project.shop.views import home, checkout, ProductDetailView, add_to_cart, remove_from_cart, \
+    remove_single_item_from_cart
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
     path('checkout/', checkout, name='checkout'),
     path('add-to-cart/<int:pk>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<int:pk>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-single-item-from-cart/<int:pk>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
 ]
--- audi_our_love_shop_project/shop/views.py
+++ audi_our_love_shop_project/shop/views.py
@@ -18,7 +18,7 @@ def home(request):
 
 @login_required
 def add_to_cart(request, pk):
-    product = get_object_or_404(Product, pk=pk)
+    product = Product.objects.get(pk=pk)
     order_product, created = OrderProduct.objects.get_or_create(
         product=product,
         user=request.user,
@@ -28,19 +28,80 @@ def add_to_cart(request, pk):
     if order_qs.exists():
         order = order_qs[0]
         # check if the order item is in the order
-        if order.items.filter(product__pk=product.pk).exists():
+        if order.items.filter(product_id=product.pk).exists():
             order_product.quantity += 1
             order_product.save()
-            return HttpResponseRedirect(request.path_info)
+            return redirect(reverse('product', kwargs={'pk': product.pk}))
         else:
             order.items.add(order_product)
+            messages.info(request, "This item was added to your cart.")
             return redirect(reverse('product', kwargs={'pk': product.pk}))
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
             user=request.user, ordered_date=ordered_date)
         order.items.add(order_product)
-        return HttpResponseRedirect(request.path_info)
+        messages.info(request, "This item was added to your cart.")
+        return redirect(reverse('product', kwargs={'pk': product.pk}))
+
+
+@login_required
+def remove_from_cart(request, pk):
+    product = Product.objects.get(pk=pk)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(product_id=product.id).exists():
+            order_product = OrderProduct.objects.filter(
+                product=product,
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_product)
+            order_product.delete()
+            messages.info(request, "This item was removed from your cart.")
+            return redirect(reverse('product', kwargs={'pk': product.pk}))
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect(reverse('product', kwargs={'pk': product.pk}))
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect(reverse('product', kwargs={'pk': product.pk}))
+
+
+@login_required
+def remove_single_item_from_cart(request, pk):
+    product = Product.objects.get(pk=pk)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(product_id=product.pk).exists():
+            order_product = OrderProduct.objects.filter(
+                product=product,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_product.quantity > 1:
+                order_product.quantity -= 1
+                order_product.save()
+            else:
+                order.items.remove(order_product)
+            messages.info(request, "This item quantity was updated.")
+            return redirect(reverse('product', kwargs={'pk': product.pk}))
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect(reverse('product', kwargs={'pk': product.pk}))
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect(reverse('product', kwargs={'pk': product.pk}))
 
 
 class ProductDetailView(DetailView):
